1e8331b2c40a29238223e97f20ce070d1f165971
Soham Sen <soham2312@gmail.com>
2019-04-22 22:59:49 +0530

    Commit_22Apr_10:59PM: Refactoring

---
+++ QueryBuilder/src/main/java/com/QueryBuilder/constants/QueryBuilderConstants.java
@@ -0,0 +1,19 @@
+package com.QueryBuilder.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryBuilderConstants {
+
+	public String BASE_QUERY = "INSERT INTO ";
+	public final String BLANK = " ";
+	public final String OPEN_PARANTHESIS = "(";
+	public final String CLOSE_PARANTHESIS = ")";
+	public final String COMMA = ",";
+	public final String VALUES = "VALUES";
+	public final String SEMI_COLON = ";";
+	public final String DOT = ".";
+	public final String VAL_SQL = "sql";
+	public final String SINGLE_QUOTE = "'";
+	public String EACH_QUERY = null;
+}
--- QueryBuilder/src/main/java/com/QueryBuilder/reader/QueryReader.java
+++ QueryBuilder/src/main/java/com/QueryBuilder/reader/QueryReader.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -14,30 +13,23 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+
+import com.QueryBuilder.constants.QueryBuilderConstants;
 
 @Service
 public class QueryReader {
 
-	private static String baseQuery = "INSERT INTO ";
-	private static final String BLANK = " ";
-	private static final String OPEN_PARANTHESIS = "(";
-	private static final String CLOSE_PARANTHESIS = ")";
-	private static final String COMMA = ",";
-	private static final String VALUES = "VALUES";
-	private static final String SEMI_COLON = ";";
-	private static final String DOT = ".";
-	private static final String VAL_SQL = "sql";
-	private static final String SINGLE_QUOTE = "'";
-	private static String eachQuery = null;
+	@Autowired
+	QueryBuilderConstants QueryBuilderConstants;
 
 	public void readQuery() {
 
 		try {
 			FileInputStream inputFile = new FileInputStream(new File("C:/Users/DELL/Desktop/Query.xlsx"));
 
-			// Create Workbook instance holding reference to .xlsx file
 			XSSFWorkbook workbook = new XSSFWorkbook(inputFile);
 
 			XSSFSheet sheet = workbook.getSheetAt(0);
@@ -47,74 +39,75 @@ public class QueryReader {
 			int numberOfFields = colNames.getPhysicalNumberOfCells();
 			int numberOfRows = sheet.getPhysicalNumberOfRows();
 			List<String> tableObj = new ArrayList<>();
-			baseQuery = baseQuery.concat(sheetName).concat(BLANK).concat(OPEN_PARANTHESIS);
+			QueryBuilderConstants.BASE_QUERY = QueryBuilderConstants.BASE_QUERY.concat(sheetName)
+					.concat(QueryBuilderConstants.BLANK).concat(QueryBuilderConstants.OPEN_PARANTHESIS);
 			IntStream.range(0, numberOfFields).forEach(colNum -> {
-				baseQuery = baseQuery.concat(colNames.getCell(colNum).toString()).concat(COMMA).concat(BLANK);
+				QueryBuilderConstants.BASE_QUERY = QueryBuilderConstants.BASE_QUERY
+						.concat(colNames.getCell(colNum).toString()).concat(QueryBuilderConstants.COMMA)
+						.concat(QueryBuilderConstants.BLANK);
 			});
 
-			baseQuery = baseQuery.substring(0, baseQuery.length() - 2);
-			baseQuery = baseQuery.concat(CLOSE_PARANTHESIS).concat(BLANK);
-			baseQuery = baseQuery.concat(VALUES).concat(BLANK);
+			this.prepareBaseQuery();
 
 			IntStream.range(2, numberOfRows).forEach(eachRowNum -> {
 				Row eachRow = sheet.getRow(eachRowNum);
-				eachQuery = OPEN_PARANTHESIS;
+				QueryBuilderConstants.EACH_QUERY = QueryBuilderConstants.OPEN_PARANTHESIS;
 				IntStream.range(0, numberOfFields).forEach(eachField -> {
 					Cell eachCell = eachRow.getCell(eachField);
 					if (colTypes.getCell(eachField).getBooleanCellValue()) {
-						eachQuery = eachQuery.concat(SINGLE_QUOTE).concat(eachCell.getStringCellValue())
-								.concat(SINGLE_QUOTE).concat(COMMA).concat(BLANK);
+						QueryBuilderConstants.EACH_QUERY = QueryBuilderConstants.EACH_QUERY
+								.concat(QueryBuilderConstants.SINGLE_QUOTE).concat(eachCell.getStringCellValue())
+								.concat(QueryBuilderConstants.SINGLE_QUOTE).concat(QueryBuilderConstants.COMMA)
+								.concat(QueryBuilderConstants.BLANK);
 					} else {
-						eachQuery = eachQuery.concat(String.valueOf((int) eachCell.getNumericCellValue())).concat(COMMA)
-								.concat(BLANK);
+						QueryBuilderConstants.EACH_QUERY = QueryBuilderConstants.EACH_QUERY
+								.concat(String.valueOf((int) eachCell.getNumericCellValue()))
+								.concat(QueryBuilderConstants.COMMA).concat(QueryBuilderConstants.BLANK);
 					}
 				});
-				eachQuery = eachQuery.substring(0, eachQuery.length() - 2);
-				eachQuery = eachQuery.concat(CLOSE_PARANTHESIS).concat(SEMI_COLON);
-				tableObj.add(baseQuery.concat(eachQuery));
-
+				this.prepareEachQuery();
+				tableObj.add(QueryBuilderConstants.BASE_QUERY.concat(QueryBuilderConstants.EACH_QUERY));
 			});
 
-			String fileName = sheetName.concat(DOT).concat(VAL_SQL);
-			File outpuFile = new File("C:/Users/DELL/Desktop/".concat(fileName));
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outpuFile));
+			this.writeSQLFile(sheetName, tableObj);
 
-			if (!CollectionUtils.isEmpty(tableObj)) {
-				tableObj.stream().forEach(eachQuery -> {
-					try {
-						writer.write(eachQuery);
-						writer.newLine();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				});
-			}
-			writer.close();
-
-			// Iterate through each rows one by one
-			Iterator<Row> rowIterator = sheet.iterator();
-			while (rowIterator.hasNext()) {
-				Row row = rowIterator.next();
-				// For each row, iterate through all the columns
-				Iterator<Cell> cellIterator = row.cellIterator();
-
-				while (cellIterator.hasNext()) {
-					Cell cell = cellIterator.next();
-					// Check the cell type and format accordingly
-					switch (cell.getCellType()) {
-					case Cell.CELL_TYPE_NUMERIC:
-						System.out.print(cell.getNumericCellValue());
-						break;
-					case Cell.CELL_TYPE_STRING:
-						System.out.print(cell.getStringCellValue());
-						break;
-					}
-				}
-				System.out.println("");
-			}
 			inputFile.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void prepareEachQuery() {
+		QueryBuilderConstants.EACH_QUERY = QueryBuilderConstants.EACH_QUERY.substring(0,
+				QueryBuilderConstants.EACH_QUERY.length() - 2);
+		QueryBuilderConstants.EACH_QUERY = QueryBuilderConstants.EACH_QUERY
+				.concat(QueryBuilderConstants.CLOSE_PARANTHESIS).concat(QueryBuilderConstants.SEMI_COLON);
+	}
+
+	private void prepareBaseQuery() {
+		QueryBuilderConstants.BASE_QUERY = QueryBuilderConstants.BASE_QUERY.substring(0,
+				QueryBuilderConstants.BASE_QUERY.length() - 2);
+		QueryBuilderConstants.BASE_QUERY = QueryBuilderConstants.BASE_QUERY
+				.concat(QueryBuilderConstants.CLOSE_PARANTHESIS).concat(QueryBuilderConstants.BLANK);
+		QueryBuilderConstants.BASE_QUERY = QueryBuilderConstants.BASE_QUERY.concat(QueryBuilderConstants.VALUES)
+				.concat(QueryBuilderConstants.BLANK);
+	}
+
+	private void writeSQLFile(String sheetName, List<String> tableObj) throws IOException {
+		String fileName = sheetName.concat(QueryBuilderConstants.DOT).concat(QueryBuilderConstants.VAL_SQL);
+		File outpuFile = new File("C:/Users/DELL/Desktop/".concat(fileName));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outpuFile));
+
+		if (!CollectionUtils.isEmpty(tableObj)) {
+			tableObj.stream().forEach(eachQuery -> {
+				try {
+					writer.write(eachQuery);
+					writer.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+		writer.close();
 	}
 }
