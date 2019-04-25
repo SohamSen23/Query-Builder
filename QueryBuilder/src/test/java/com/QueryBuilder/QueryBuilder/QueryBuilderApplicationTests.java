df2b64d6adb4ff669c426d29e8c7063b8f1b4a35
Soham Sen <soham2312@gmail.com>
2019-04-25 23:32:37 +0530

    Commit_25Apr_11:32PM: Test case framework added. Needs enhancement

--- QueryBuilder/src/test/java/com/QueryBuilder/QueryBuilder/QueryBuilderApplicationTests.java
+++ QueryBuilder/src/test/java/com/QueryBuilder/QueryBuilder/QueryBuilderApplicationTests.java
@@ -1,16 +1,34 @@
 package com.QueryBuilder.QueryBuilder;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.QueryBuilder.QueryBuilder.constants.TestConstants;
+import com.QueryBuilder.reader.QueryReader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(profiles = "localtest")
 public class QueryBuilderApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testReadQuery() {
+		QueryReader mockQueryReader = Mockito.mock(QueryReader.class);
+		ReflectionTestUtils.setField(mockQueryReader, "inputFile", getClass().getResource(TestConstants.TEST_QUERY).getFile());
+		ReflectionTestUtils.invokeMethod(mockQueryReader, "readQuery");
+		Object mockTableObj = ReflectionTestUtils.getField(mockQueryReader, "tableObj");
+		assertNotNull(mockTableObj);
 	}
 
 }
---
+++ QueryBuilder/src/test/java/com/QueryBuilder/QueryBuilder/constants/TestConstants.java
@@ -0,0 +1,6 @@
+package com.QueryBuilder.QueryBuilder.constants;
+
+public class TestConstants {
+
+	public final static String TEST_QUERY = "Query.xlsx";
+}
